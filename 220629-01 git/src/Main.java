b0eb936da621c308709b5f2f5f6ba2c321808de3
Administrator <Administrator@SC-202106161522>
2022-06-29 10:19:47 +0900

    push 연습중입니다~

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		// 헬로 월드 완성
 		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		
+		// push 연습
+		System.out.println("push push ~~~~~~~~~~~~~~");
 	}
 }
